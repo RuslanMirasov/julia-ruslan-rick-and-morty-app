3d927c15e508edf9f5dcf4eac7a48fb318ff52f0
Ruslan Mirasov <info@mirasov.dev>
2024-08-21 16:02:10 +0200

    add search-bar component

--- components/search-bar/search-bar.js
+++ components/search-bar/search-bar.js
@@ -6,3 +6,35 @@ export const getSerchQuery = (e) => {
   form.reset();
   return searchQuery;
 };
+
+export const SearchBar = (onSubmit) => {
+  const searchForm = document.createElement("form");
+  searchForm.classList.add("search-bar");
+  searchForm.setAttribute("action", "");
+  searchForm.setAttribute("data-js", "search-bar");
+
+  const inputForm = document.createElement("input");
+  inputForm.classList.add("search-bar__input");
+  inputForm.setAttribute("name", "query");
+  inputForm.setAttribute("type", "text");
+  inputForm.setAttribute("placeholder", "search characters");
+  inputForm.setAttribute("aria-label", "character name");
+
+  const searchButton = document.createElement("button");
+  searchButton.classList.add("search-bar__button");
+  searchButton.setAttribute("aria-label", "search for character");
+
+  const searchButtonImage = document.createElement("img");
+  searchButtonImage.classList.add("search-bar__icon");
+  searchButtonImage.setAttribute("src", "assets/magnifying-glass.png");
+  searchButtonImage.setAttribute("alt", "Rick & Morty");
+
+  searchButton.append(searchButtonImage);
+  searchForm.append(inputForm, searchButton);
+
+  if (onSubmit) {
+    searchForm.addEventListener("submit", onSubmit);
+  }
+
+  return searchForm;
+};
